739f70b10d04a168f8ae0ff66b0fdd0d11e24a86
nijuyonkadesu <24ksriramv@gmail.com>
2022-11-10 20:50:55 +0530

    boilerplate viewmodel + factory

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.inventory.data.ItemDao
+
+class InventoryViewModel(private val itemDao: ItemDao): ViewModel(){
+
+}
+class InvenventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(InventoryViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
